7a0af3e9ee766b1630afdb2f816225d750cfd897
Robert Dumitru <72272382+robert-dumitru@users.noreply.github.com>
2022-11-21 21:08:38 -0300

    fixed async utils code

--- app/util.py
+++ app/util.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 import uuid
 import itertools
+from io import BytesIO
 import openai
 import replicate
 import requests
@@ -17,7 +18,6 @@ stable_diffusion_model = replicate.models.get("stability-ai/stable-diffusion")
 clip_caption_model = replicate.models.get("rmokady/clip_prefix_caption")
 
 
-# TODO: add more async processing
 async def generate_images(images: list[Image]) -> list[Image]:
     """
     Abstraction of image generation pipeline.
@@ -26,7 +26,12 @@ async def generate_images(images: list[Image]) -> list[Image]:
     :return: extension of image set.
     """
     # captioning list of images
-    clip_outputs = await asyncio.gather(*map(lambda i: clip_caption_model.predict(i)["title"], images))
+    async def caption_image(image: Image) -> str:
+        file = BytesIO()
+        image.save(file, "PNG")
+        return clip_caption_model.predict(image=file, model="conceptual-captions", use_beam_search=True)
+
+    clip_outputs = await asyncio.gather(*map(caption_image, images))
     captions = "".join(pred.strip() + "\n" for pred in clip_outputs)
     logging.debug(f"Image captions: {captions}")
 
@@ -40,21 +45,23 @@ async def generate_images(images: list[Image]) -> list[Image]:
     diffusion_prompts = list(itertools.chain(*[p["text"].splitlines() for p in gpt3_response["choices"]]))
     logging.debug(f"Diffusion prompts: {diffusion_prompts}")
 
+    async def download_image(url: str) -> Image:
+        return Image.open(requests.get(url, stream=True).raw)
+
     # generate new images using stable diffusion
     async def create_image(prompt: str) -> list[Image]:
-        outputs = await stable_diffusion_model.predict(prompt=prompt, width=512, height=512)
-        image_list = await asyncio.gather(lambda url: Image.open(requests.get(url, stream=True).raw), outputs)
+        outputs = stable_diffusion_model.predict(prompt=prompt, width=512, height=512)
+        image_list = await asyncio.gather(*map(download_image, outputs))
         return image_list
 
-    new_images = await asyncio.gather(create_image, diffusion_prompts)
-    return new_images
+    new_images = await asyncio.gather(*map(create_image, diffusion_prompts))
+    return list(itertools.chain(*new_images))
 
 
 # script to use for testing
-# TODO: adapt with async code
 if __name__ == "__main__":
-    test_dir = "../photos"
-    save_dir = "../results"
+    test_dir = "app/photos"
+    save_dir = "app/results"
     original_images = []
     for path in os.listdir(test_dir):
         image = Image.open(f"{test_dir}/{path}")
